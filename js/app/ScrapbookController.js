026517a7ff61bb676449256cd0d8ce194c245c53
kellyluo25 <kellyluo25@gmail.com>
2019-01-23 19:40:49 -0500

    Update ScrapbookController.js

--- js/app/ScrapbookController.js
+++ js/app/ScrapbookController.js
@@ -4,6 +4,5 @@ app.controller('ScrapbookController', ['$scope', 'entries',
     function($scope, entries) {
       entries.then(function(data) {
         $scope.entries = data;
-        console.log(data)
       });  
 }]);
