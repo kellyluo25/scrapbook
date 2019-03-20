ea83d6ac3f61a7f1fec2bf9938f2f2bffb74d729
kelly luo <kellyluo25@gmail.com>
2019-03-20 18:18:46 -0400

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -2,7 +2,6 @@ var app = angular.module('ScrapbookApp', ['ngRoute']);
 
 app.config(function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
-  $locationProvider.html5Mode(true);
   $routeProvider
   .when('/', {
     templateUrl : 'js/app/entry.html',
