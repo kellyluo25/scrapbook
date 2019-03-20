bf5d12347c080141d736f03cf150377a36f56593
kelly luo <kellyluo25@gmail.com>
2019-03-20 18:01:13 -0400

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -6,7 +6,7 @@ app.config(function ($routeProvider,$locationProvider) {
     templateUrl : 'js/app/entry.html',
     controller  : 'ScrapbookController'
   })
-.otherwise({
+ .otherwise({
       redirectTo: '/'
   })
   
