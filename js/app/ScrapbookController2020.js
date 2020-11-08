49311f7ddd2cc1d7d079382787df01bd7b69fb81
kellyluo <kellyluo25@gmail.com>
2020-11-07 21:47:45 -0500

    it's extensible now!
    coding @ scale :hearts:

--- js/app/ScrapbookController2020.js
+++ js/app/ScrapbookController2020.js
@@ -4,7 +4,24 @@
 app2020.controller('ScrapbookController2020', ['$scope', 'entries',
     function($scope, entries) {
       entries.then(function(data) {
+        // set 'entries' as data from controller 
         $scope.entries = data.records;
+          
         console.log($scope.entries);
+        
+        function mediaCount(media_type) {
+            return ($scope.entries.filter(entry => entry.fields.media === media_type)).length;
+        };
+        
+        // set 'media totals' as counts for every media type
+        $scope.media_totals = {
+            "all": $scope.entries.length,
+            "shows": mediaCount("shows"), 
+            "films": mediaCount("films"),
+            "books": mediaCount("books"),
+            "audio": mediaCount("audio"),
+        };
+        
+        console.log($scope.media_totals);
     });  
 }]);
